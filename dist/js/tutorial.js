47fcba49dbb6f40ed998983ceceb703930488753
krysceejay <chrischimachijioke@gmail.com>
2020-06-24 13:27:59 +0100

    working on details page

---
+++ dist/js/tutorial.js
@@ -0,0 +1,62 @@
+// var x = 1;
+// var y = 2;
+
+// Variable types
+
+const x = 1; //Integer
+const y = 2; //Integer
+
+const z = "Divine"; //String
+
+const d = true; //Bolean
+const f = false; //Bolean
+
+const a = 2.6; //Float
+
+const b = 2.6777; //Double
+
+// Concatenation
+//console.log(x + y + z);
+const myFirstName = "Divine";
+const myLastName = "John";
+
+const myFullName = myFirstName + " " + myLastName;
+
+//Operations
+// console.log(x + y); //Addition
+// console.log(x - y); //Subtraction
+// console.log(x / y); //Division
+// console.log(x * y); //Multiplication
+
+// == / === for equality
+// < less than
+// > greater than
+// <= less than or equal to
+// >= greater than or equal to
+
+// & and means all should be true
+// || or means one should be true
+
+// One equal to means assignment;
+// Two equal to means check;
+//Conditionals
+const p = 10;
+const r = 20;
+
+if (p == 10 || r == 12) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
+
+// Loop
+// For loop
+// foreach loop
+
+const num = 10;
+
+for (i = 1; i < num; i++) {
+  console.log(i);
+}
+
+//const z = x + y;
